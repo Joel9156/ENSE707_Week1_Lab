5360474bc9157151459eb5e3cc2f461cfacd8104
Joel Lee <ljeljelje9156@gmail.com>
2026-07-15 13:33:59 +1200

    Add BankAccount class and unit test project

--- ENSE707_Week1_Lab/Program.cs
+++ ENSE707_Week1_Lab/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace ENSE707_Week1_Lab
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!!");
-        }
-    }
-}
+﻿using ENSE707_Week1_Lab;
+
+BankAccount account = new BankAccount("Student User", 100);
+
+account.Deposit(50);
+account.Withdraw(30);
+
+Console.WriteLine($"Account holder: {account.AccountHolder}");
+Console.WriteLine($"Current balance: {account.Balance}");
+Console.WriteLine($"Fee on $100 transaction: {account.CalculateTransactionFee(100)}");
