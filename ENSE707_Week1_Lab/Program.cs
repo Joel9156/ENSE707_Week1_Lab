ef44453c32791ef4da7ab3644fa6ac70b9c94d8c
Joel Lee <ljeljelje9156@gmail.com>
2026-07-15 12:45:01 +1200

    Add project files.

---
+++ ENSE707_Week1_Lab/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ENSE707_Week1_Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
